c0fcd1e9af49c5bd9d06c562b587a4d1c745b88e
hugobeguier <55714788+hugobeguier@users.noreply.github.com>
2020-10-26 18:01:37 +0100

    Added LoadLevelState

--- Engine/InGameState.cpp
+++ Engine/InGameState.cpp
@@ -2,17 +2,8 @@
 #include "InGameState.h"
 #include "StateStack.h"
 #include "Scene.h"
-#include "LevelLoader.h"
 #include "GameObject.h"
-#include "TransformComponent.h"
-#include "ModelComponent.h"
-#include "ModelFactory.h"
-#include "Model.h"
-#include "CameraFactory.h"
-#include "Camera.h"
-#include "LightFactory.h"
-#include "EnvironmentLight.h"
-#include <CUnityLoader.h> //In order to know the size of ObjectData
+
 
 CInGameState::CInGameState(CStateStack& aStateStack) : CState(aStateStack)
 {
@@ -26,29 +17,6 @@ CInGameState::~CInGameState()
 
 void CInGameState::Awake()
 {
-	//Push Load Level State for Level (Saved somewhere)
-	CLevelLoader levelLoader;// = //std::make_unique<CLevelLoader>();
-	levelLoader.Init();
-	levelLoader.LoadNewLevel("Levels/SampleScene");
-
-	CGameObject* player = new CGameObject();
-	player->AddComponent<CTransformComponent>(*player);
-	auto model = player->AddComponent<CModelComponent>(*player);
-
-	CModel* playerModel = CModelFactory::GetInstance()->GetModelPBR("Assets/3D/Character/MainCharacter/CH_PL_Daughter_01_19G4_1_19.fbx");
-	model->SetMyModel(playerModel);
-
-	CCamera* camera = CCameraFactory::GetInstance()->CreateCamera(65.0f);
-
-	CEnvironmentLight* environmentLight = CLightFactory::GetInstance()->CreateEnvironmentLight("Yokohama2.dds");
-
-	CScene::GetInstance()->AddInstance(environmentLight);
-	CScene::GetInstance()->AddInstance(player);
-	CScene::GetInstance()->AddInstance(camera);
-	CScene::GetInstance()->SetMainCamera(camera);
-	//When Load Level is Complete -> LoadLevelState will Pop itself and then this Awake will continue.
-
-
 	for (auto& gameObject : CScene::GetInstance()->myGameObjects)
 	{
 		gameObject->Awake();
