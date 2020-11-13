95dc6560e093ace7f8f712adfc2418d177ed795d
Humbabah <haqvin.bager@gmail.com>
2020-11-13 15:40:31 +0100

    Hej

--- Engine/EnemyFactory.cpp
+++ Engine/EnemyFactory.cpp
@@ -36,7 +36,7 @@ CGameObject& CEnemyFactory::CreateEnemy(DirectX::SimpleMath::Vector3 aPosition, 
 	enemy->myTransform->Position(aPosition);
 	enemy->myTransform->Rotation({ 0.0f, 0.0f, 0.0f });
 
-	enemy->AddComponent<CCapsuleColliderComponent>(*enemy, 0.5f, 2.0f);
+	enemy->AddComponent<CCircleColliderComponent>(*enemy, 0.5f, ECollisionLayer::ENEMY);
 	/*auto modelComponent =*/ enemy->AddComponent<CModelComponent>(*enemy, "Assets/3D/Character/CH_NPC_enemy_01_19G4_1_19/CH_NPC_enemy_01_19G4_1_19.fbx");
 
 	enemy->AddComponent<CStatsComponent>(*enemy, aHealth, aDamage, aMoveSpeed, aCooldown);
