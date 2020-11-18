7929ed1eb24c3a94828d7e3244e56f23784f0831
Humbabah <haqvin.bager@gmail.com>
2020-11-18 18:14:04 +0100

    Canvas

--- Engine/Canvas.cpp
+++ Engine/Canvas.cpp
@@ -1,37 +1,120 @@
 #include "stdafx.h"
 #include "Canvas.h"
 #include "MainSingleton.h"
+#include "PostMaster.h"
 #include "Button.h"
+#include "SpriteInstance.h"
+#include "TextInstance.h"
+#include "AnimatedUIElement.h"
 
-CCanvas::CCanvas(std::vector<EMessageType> someMessageTypes) : myMessageTypes(someMessageTypes)
+CCanvas::CCanvas(std::vector<EMessageType> someMessageTypes) : myMessageTypes(someMessageTypes), myBackground(nullptr)
 {
-	SubscribeToEvents();
+	SubscribeToMessages();
 }
 
 CCanvas::~CCanvas()
 {
-	UnsubscribeToEvents();
+	UnsubscribeToMessages();
 	myMessageTypes.clear();
 }
 
-//void CCanvas::Init()
-//{
-//}
+void CCanvas::Init()
+{
+}
 
 void CCanvas::Update(float /*aDeltaTime*/)
 {
 }
 
-void CCanvas::SubscribeToEvents()
+void CCanvas::Receive(const SMessage& aMessage)
+{
+	switch (aMessage.myMessageType)
+	{
+	case EMessageType::ColliderAdded:
+		break;
+	case EMessageType::ColliderRemoved:
+		break;
+	case EMessageType::EnemyDied:
+		break;
+	case EMessageType::MainMenu:
+		break;
+	case EMessageType::Count:
+		break;
+	default:
+		break;
+	}
+}
+
+void CCanvas::SubscribeToMessages()
 {
 	for (auto messageType : myMessageTypes) {
 		CMainSingleton::PostMaster().Subscribe(messageType, this);
 	}
 }
 
-void CCanvas::UnsubscribeToEvents()
+void CCanvas::UnsubscribeToMessages()
 {
 	for (auto messageType : myMessageTypes) {
 		CMainSingleton::PostMaster().Unsubscribe(messageType, this);
 	}
+}
+
+bool CCanvas::GetEnabled()
+{
+	return myIsEnabled;
+}
+
+CSpriteInstance* CCanvas::GetBackground()
+{
+	return myBackground;
+}
+
+std::vector<CAnimatedUIElement*> CCanvas::GetAnimatedUIs()
+{
+	return myAnimatedUIs;
+}
+
+std::vector<CButton*> CCanvas::GetButtons()
+{
+	return myButtons;
+}
+
+std::vector<CSpriteInstance*> CCanvas::GetSprites()
+{
+	return mySprites;
+}
+
+std::vector<CTextInstance*> CCanvas::GetTexts()
+{
+	return myTexts;
+}
+
+void CCanvas::SetEnabled(bool isEnabled)
+{
+	myIsEnabled = isEnabled;
+}
+
+void CCanvas::SetBackground(CSpriteInstance* aBackground)
+{
+	myBackground = aBackground;
+}
+
+void CCanvas::AddAnimatedUI(CAnimatedUIElement* anAnimatedUIElement)
+{
+	myAnimatedUIs.emplace_back(anAnimatedUIElement);
+}
+
+void CCanvas::AddButton(CButton* aButton)
+{
+	myButtons.emplace_back(aButton);
+}
+
+void CCanvas::AddSprite(CSpriteInstance* aSprite)
+{
+	mySprites.emplace_back(aSprite);
+}
+
+void CCanvas::AddText(CTextInstance* aText)
+{
+	myTexts.emplace_back(aText);
 }
--- Engine/Canvas.h
+++ Engine/Canvas.h
@@ -2,21 +2,49 @@
 #include "Observer.h"
 
 class CButton;
-
+class CSpriteInstance;
+class CTextInstance;
+class CAnimatedUIElement;
 class CCanvas : public IObserver
 {
 public:
 	CCanvas(std::vector<EMessageType> someMessageTypes);
 	~CCanvas();
 
-	//void Init();
-
+public:
+	void Init();
 	void Update(float aDeltaTime);
 
-	void SubscribeToEvents();
-	void UnsubscribeToEvents();
-private:
-	std::vector<CButton*> myButtons;
+public:
+	void Receive(const SMessage& aMessage) override;
+	void SubscribeToMessages();
+	void UnsubscribeToMessages();
 
+public:
+	bool GetEnabled();
+
+	CSpriteInstance* GetBackground();
+	std::vector<CAnimatedUIElement*> GetAnimatedUIs();
+	std::vector<CButton*> GetButtons();
+	std::vector<CSpriteInstance*> GetSprites();
+	std::vector<CTextInstance*> GetTexts();
+
+public:
+	void SetEnabled(bool isEnabled);
+
+	void SetBackground(CSpriteInstance* aBackground);
+	void AddAnimatedUI(CAnimatedUIElement* anAnimatedUIElement);
+	void AddButton(CButton* aButton);
+	void AddSprite(CSpriteInstance* aSprite);
+	void AddText(CTextInstance* aText);
+
+private:
+	bool myIsEnabled;
+	CSpriteInstance* myBackground;
+
+	std::vector<CAnimatedUIElement*> myAnimatedUIs;
+	std::vector<CButton*> myButtons;
+	std::vector<CSpriteInstance*> mySprites;
+	std::vector<CTextInstance*> myTexts;
 	std::vector<EMessageType> myMessageTypes;
 };
