1f811c16b57574c92714780977d6280f5daf2d6c
PapaGreger <72140147+PapaGreger@users.noreply.github.com>
2020-11-18 18:11:23 +0100

    Button is lambda

--- Engine/Button.h
+++ Engine/Button.h
@@ -1,10 +1,35 @@
 #pragma once
-class CButton {
-public:
-	CButton();
-	~CButton();
+#include <functional>
 
-private:
+class CCanvas;
+class CSpriteInstace;
 
+enum class EButtonState {
+	Idle,
+	Hover,
+	Click
 };
 
+class CButton {
+	friend CCanvas;
+public:
+	void OnHover();
+	template<typename...Params>
+	void OnClick(Params&&...p);
+	void OnLeave();
+
+//private:
+	CButton(std::function<void()> aFunction);
+	~CButton();
+
+	bool myEnabled;
+	EButtonState myState;
+	std::function<void()> myFunction;
+	std::array<CSpriteInstace*, 3> mySprites;
+};
+
+template<typename ...Params>
+inline void CButton::OnClick(Params&& ...p)
+{
+	myFunction(std::forward<Params>(p)...);
+}
