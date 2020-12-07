05e52fcb29b0333c88728236d88a4998596a78e7
Nicolas Risberg <35574154+nicolas-risberg@users.noreply.github.com>
2020-12-07 09:08:11 +0100

    Custom cursor is used, not sure how to load it generally for both groups though

--- Engine/WindowHandler.cpp
+++ Engine/WindowHandler.cpp
@@ -40,7 +40,7 @@ bool CWindowHandler::Init(CWindowHandler::SWindowData someWindowData)
 {
     myWindowData = someWindowData;
 
-    //HCURSOR customCursor = LoadCursorFromFileW(L"Assets/3D/UI/UI_In_Cursor_01_19G4.cur");
+    HCURSOR customCursor = LoadCursorFromFileW(L"Assets/3D/UI/Ingame/UI_In_Cursor_01_19G4.cur");
 
     WNDCLASSEX windowclass = {};
     windowclass.cbSize = sizeof(WNDCLASSEX);
@@ -50,7 +50,7 @@ bool CWindowHandler::Init(CWindowHandler::SWindowData someWindowData)
     windowclass.cbWndExtra = 0;
     windowclass.hInstance = GetModuleHandle(nullptr);
     windowclass.hIcon = (HICON)LoadImage(NULL, L"ironwrought.ico", IMAGE_ICON, 0, 0, LR_LOADFROMFILE | LR_DEFAULTSIZE);
-    windowclass.hCursor = /*customCursor*/LoadCursor(nullptr, IDC_ARROW);
+    windowclass.hCursor = customCursor;
     windowclass.lpszClassName = L"3DEngine";
     RegisterClassEx(&windowclass);
 
