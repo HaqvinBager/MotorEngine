324f5bd2b7e46cff15770a55230628220a716409
AxelSavage <axelsavage@gmail.com>
2020-12-14 14:32:29 +0100

    Uses DebugLevel.json now (Change this in LoadLevelState if you want the final Build Version)

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -23,21 +23,38 @@ CGame::~CGame()
 
 void CGame::Init()
 {
-	myStateStack.Awake(
-		{ 
-		CStateStack::EState::BootUpState,
-		CStateStack::EState::TitleScreenState,
-		CStateStack::EState::MainMenu, 
-		CStateStack::EState::IntroState,
-		CStateStack::EState::LoadLevel,
-		CStateStack::EState::InGame,
-		CStateStack::EState::PauseMenu 
-		}, 
-		CStateStack::EState::BootUpState);
+	InitDev();
+	//InitRealGame();
 }
 
 bool CGame::Update()
 {
 	return myStateStack.Update();
+}
+
+void CGame::InitDev()
+{
+	myStateStack.Awake(
+		{
+			CStateStack::EState::LoadLevel,
+			CStateStack::EState::InGame,
+			CStateStack::EState::PauseMenu
+		},
+		CStateStack::EState::LoadLevel);
+}
+
+void CGame::InitRealGame()
+{
+	myStateStack.Awake(
+		{
+			CStateStack::EState::BootUpState,
+			CStateStack::EState::TitleScreenState,
+			CStateStack::EState::MainMenu, 
+			CStateStack::EState::IntroState,
+			CStateStack::EState::LoadLevel,
+			CStateStack::EState::InGame,
+			CStateStack::EState::PauseMenu
+		},
+		CStateStack::EState::BootUpState);
 }
 
